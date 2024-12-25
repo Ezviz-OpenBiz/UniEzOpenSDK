8d695dc2f7221687663c335804eba93a1f41f3bc
XuXinYu <1019416676@qq.com>
2024-12-25 20:43:41 +0800

    ADD：iOS原生Uniapp插件发布

---
+++ ios/super_ezviz.framework/Headers/super_ezviz.h
@@ -0,0 +1,18 @@
+//
+//  super_ezviz.h
+//  super_ezviz
+//
+//  Created by yangshuang3 on 2024/7/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for super_ezviz.
+FOUNDATION_EXPORT double super_ezvizVersionNumber;
+
+//! Project version string for super_ezviz.
+FOUNDATION_EXPORT const unsigned char super_ezvizVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <super_ezviz/PublicHeader.h>
+
+
