5a23bb821300c7d205bed94ad9627819f8b5d3ec
XuXinYu <1019416676@qq.com>
2025-06-24 11:42:02 +0800

    update SDK

---
+++ ios/EZOpenSDKFramework.framework/Headers/EZOpenSDKFramework.h
@@ -0,0 +1,55 @@
+//
+//  EZOpenSDKFramework.h
+//  EZOpenSDKFramework
+//
+//  Created by JuneCheng on 2022/7/23.
+//
+
+#ifndef EZOpenSDKFramework_h
+#define EZOpenSDKFramework_h
+
+#import <EZOpenSDKFramework/EZAccessToken.h>
+#import <EZOpenSDKFramework/EZAlarmInfo.h>
+#import <EZOpenSDKFramework/EZAreaInfo.h>
+#import <EZOpenSDKFramework/EZCameraInfo.h>
+#import <EZOpenSDKFramework/EZCloudRecordDownloadTask.h>
+#import <EZOpenSDKFramework/EZCloudRecordFile.h>
+#import <EZOpenSDKFramework/EZDetectorInfo.h>
+#import <EZOpenSDKFramework/EZDeviceCloudServiceInfo.h>
+#import <EZOpenSDKFramework/EZDeviceInfo.h>
+#import <EZOpenSDKFramework/EZDevicePtzAngleInfo.h>
+#import <EZOpenSDKFramework/EZDeviceRecordDownloadTask.h>
+#import <EZOpenSDKFramework/EZDeviceRecordFile.h>
+#import <EZOpenSDKFramework/EZDeviceUpgradeStatus.h>
+#import <EZOpenSDKFramework/EZDeviceVersion.h>
+#import <EZOpenSDKFramework/EZHCNetDeviceInfo.h>
+#import <EZOpenSDKFramework/EZHiddnsDeviceInfo.h>
+#import <EZOpenSDKFramework/EZLeaveMessage.h>
+#import <EZOpenSDKFramework/EZPlayerExParamInfo.h>
+#import <EZOpenSDKFramework/EZProbeDeviceInfo.h>
+#import <EZOpenSDKFramework/EZRecordDownloader.h>
+#import <EZOpenSDKFramework/EZRecordDownloadTask.h>
+#import <EZOpenSDKFramework/EZStorageInfo.h>
+#import <EZOpenSDKFramework/EZSubDeviceInfo.h>
+#import <EZOpenSDKFramework/EZTokenKit.h>
+#import <EZOpenSDKFramework/EZUserInfo.h>
+#import <EZOpenSDKFramework/EZVideoQualityInfo.h>
+#import <EZOpenSDKFramework/EZVideoTransformer.h>
+#import <EZOpenSDKFramework/EzvizRecordFileInfo.h>
+#import <EZOpenSDKFramework/EzvizWatchServerInfo.h>
+
+#import <EZOpenSDKFramework/EZSADPDeviceInfo.h>
+#import <EZOpenSDKFramework/EZAPDevInfo.h>
+#import <EZOpenSDKFramework/EZConfigTokenInfo.h>
+#import <EZOpenSDKFramework/EZWiFiItemInfo.h>
+
+#import <EZOpenSDKFramework/EZConstants.h>
+#import <EZOpenSDKFramework/EZConstants_Auth.h>
+#import <EZOpenSDKFramework/EZConstants_WifiConfig.h>
+#import <EZOpenSDKFramework/EZOpenSDK.h>
+#import <EZOpenSDKFramework/EZGlobalSDK.h>
+#import <EZOpenSDKFramework/EZHCNetDeviceSDK.h>
+#import <EZOpenSDKFramework/EZPlayer.h>
+#import <EZOpenSDKFramework/EZStreamPlayer.h>
+
+#endif /* EZOpenSDKFramework_h */
---
+++ ios/EZOpenSDKFramework.framework/Headers/EZVideoTalkComponent.h
@@ -0,0 +1,23 @@
+//
+//  EZVideoTalkComponent.h
+//  EZOpenSDK
+//
+//  Created by JuneCheng on 2022/7/23.
+//
+
+#ifndef EZVideoTalkComponent_h
+#define EZVideoTalkComponent_h
+
+#import <EZOpenSDKFramework/EZBAVParam.h>
+#import <EZOpenSDKFramework/EZConferenceClient.h>
+#import <EZOpenSDKFramework/EZRTCCameraParam.h>
+#import <EZOpenSDKFramework/EZRTCMediaSessionDefines.h>
+#import <EZOpenSDKFramework/EZRTCSampleHandleManager.h>
+#import <EZOpenSDKFramework/EZRTCVideoEncodeParam.h>
+#import <EZOpenSDKFramework/EZVideoTalk.h>
+#import <EZOpenSDKFramework/EZVideoTalkGlobal.h>
+#import <EZOpenSDKFramework/EZVideoTalkSDK.h>
+#import <EZOpenSDKFramework/EZVideoTalkSDKDefines.h>
+#import <EZOpenSDKFramework/EZVideoTalkView.h>
+
+#endif /* EZVideoTalkComponent_h */
